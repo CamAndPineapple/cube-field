aa06877a1e1d430d83620b411ced2cdb4554fc94
Camden Rudisill <camden@planet.com>
2016-10-27 16:15:34 -0700

    cleanup and controls added

--- js/random-cubes.js
+++ js/random-cubes.js
@@ -1,6 +1,6 @@
 let canvas = document.getElementById('canvas');
 
-// SETUP CONSTANTS
+// CONSTANTS
 const HEIGHT = window.innerHeight;
 const WIDTH = window.innerWidth;
 const FOV = 45; /* Camera frustum vertical field of view (degrees) */
@@ -9,13 +9,13 @@ const NEAR = 0.1; /* Camera frustum near plane (degrees)*/
 const FAR = 1000; /* Camera frustum far plane (degrees)*/
 
 // CAMERA CONSTANTS
-const CAMERA_X = -90;
-const CAMERA_Y = 50;
-const CAMERA_Z = 100;
+const CAMERA_X = -380;
+const CAMERA_Y = 120;
+const CAMERA_Z = 375;
 
 // PLANE CONSTANTS
-const PLANE_WIDTH = 60;
-const PLANE_HEIGHT = 40;
+const PLANE_WIDTH = 600;
+const PLANE_HEIGHT = 400;
 
 // PARTICLE CONTANTS
 const MAX_PARTICLES = 100;
@@ -27,49 +27,60 @@ class Particle {
 		this.w = width || Math.random() * 10;
 		this.h = height || Math.random() * 10;
 		this.d = depth || Math.random() * 10;
-		this.color = Math.random() * 0xffffff;
+		this.color = 0xffffff;
 	}
 
-	setPosX(maxWidth) {
+	setMaxPosX(maxWidth) {
 		return -maxWidth/2 + (Math.random() * maxWidth);
 	}
 
-	setPosY(maxHeight) {
+	setMaxPosY(maxHeight) {
 		return Math.random() * maxHeight;
 	}
 
-	setPosZ(maxDepth) {
+	setMaxPosZ(maxDepth) {
 		return -maxDepth/2 + (Math.random() * maxDepth);
 	}
 }
 
 let scene = new THREE.Scene();
-//scene.fog = new THREE.Fog( 0xeeeeee, 60, 100);
+
 let camera = new THREE.PerspectiveCamera(FOV, ASPECT_RATIO, NEAR, FAR);
 camera.position.x = CAMERA_X;
 camera.position.y = CAMERA_Y;
 camera.position.z = CAMERA_Z;
 
-const OrbitControls = new THREE.OrbitControls(camera);
-const axisHelpers = new THREE.AxisHelper(30);
+let ambiColor = "#0c0c0c";
+let ambientLight = new THREE.AmbientLight(ambiColor);
+scene.add(ambientLight);
 
-let planeGeometry = new THREE.PlaneGeometry(PLANE_WIDTH,PLANE_HEIGHT,1,1);
-let planeMaterial = new THREE.MeshLambertMaterial({color: 0x4b964b});
+let topLeftPointLight = new THREE.PointLight(0x0033ff, 3, 200);   
+topLeftPointLight.position.set(-PLANE_WIDTH/4, 5, -PLANE_HEIGHT/4);
+scene.add(topLeftPointLight);
+
+let bottomLeftPointLight= new THREE.PointLight(0xFF0000, 3, 200); 
+bottomLeftPointLight.position.set(-PLANE_WIDTH/4, 5, PLANE_HEIGHT/4);
+scene.add(bottomLeftPointLight);
+
+let topRightPointLight = new THREE.PointLight(0xFFFF00, 3, 200); 
+topRightPointLight.position.set(PLANE_WIDTH/4, 5, -PLANE_HEIGHT/4);
+scene.add(topRightPointLight);
+
+let bottomRightPointLight = new THREE.PointLight(0xff00ff, 3, 200); 
+bottomRightPointLight.position.set(PLANE_WIDTH/4, 5, PLANE_HEIGHT/4);
+scene.add(bottomRightPointLight);
+
+let spotLight = new THREE.SpotLight(0xffffff);
+spotLight.position.set(-80, 120, -10);
+spotLight.castShadow = true;
+scene.add(spotLight);
+
+const planeGeometry = new THREE.PlaneGeometry(PLANE_WIDTH,PLANE_HEIGHT,1,1);
+const planeMaterial = new THREE.MeshLambertMaterial({color: 0x4b964b});
 let plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.receiveShadow = true;
 plane.rotation.x = -Math.PI / 2; 
-plane.position.x = 0;
-plane.position.y = 0;
-plane.position.z = 0;
 plane.name = 'plane';
-
-let ambientLight = new THREE.AmbientLight(0xeeeeee);
-scene.add(ambientLight);
-
-let spotLight = new THREE.SpotLight(0xffffff);
-spotLight.position.set(-40, 60, -10);
-spotLight.castShadow = true;
-scene.add(spotLight);
 
 let renderer = new THREE.WebGLRenderer();
 renderer.setClearColor(new THREE.Color(0x0a0a21));
@@ -77,26 +88,38 @@ renderer.setSize(WIDTH, HEIGHT);
 renderer.shadowMap.enabled = true;
 canvas.appendChild(renderer.domElement);
 
-let controls = new function() {
+const OrbitControls = new THREE.OrbitControls(camera);
+const axisHelper = new THREE.AxisHelper(300);
+const cameraHelper = new THREE.CameraHelper(spotLight.shadow.camera);
 
+// CONTROLS ====================================================================
+
+let controls = new function() {
 	this.cubeRotSpd = 0.01;
-	this.expWidth = 1;
+	this.expand = 1;
+	this.ambientLightON = true;
+	this.spotLightON = true;
+	this.ambientColor = ambiColor;
 	this.particleCount = cubes.length;
 	this.sinWaveFreq = 0.0;
 	this.showAxes = false;
-	this.showPlane = false;
+	this.showPlane = true;
+	this.showSpotLight = false;
+	this.rotateCamera = false;
+	this.exploding = false;
+	this.lastIncrement = 0;
 
 	this.addCubes = function () {	
 		for (let i = 0; i < MAX_PARTICLES; i++) {
 			const particle = new Particle();
-			let cubeGeometry = new THREE.CubeGeometry(particle.w, particle.h, particle.d);
-			let cubeMaterial = new THREE.MeshLambertMaterial({color: particle.color });
+			const cubeGeometry = new THREE.CubeGeometry(particle.w, particle.h, particle.d);
+			const cubeMaterial = new THREE.MeshLambertMaterial({color: particle.color });
 			let cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 			cube.castShadow = true;
 			cube.name = `cube-${scene.children.length - 3}`;
-			cube.position.x = particle.setPosX(PLANE_WIDTH); 
-			cube.position.y = particle.setPosY(10);
-			cube.position.z = particle.setPosZ(PLANE_HEIGHT);
+			cube.position.x = particle.setMaxPosX(PLANE_WIDTH); 
+			cube.position.y = particle.setMaxPosY(100);
+			cube.position.z = particle.setMaxPosZ(PLANE_HEIGHT);
 			cubes.push(cube);
 			scene.add(cube);
 		}
@@ -127,66 +150,79 @@ let controls = new function() {
 		cubes.length = 0;
 		this.particleCount = cubes.length;
 	};
-
-	this.explode = function () {
-		let multiple = 1;
-		while (multiple < 1.05) {
-
-            scene.traverse(obj => {
-                if (obj instanceof THREE.Mesh && obj != plane) {
-                    if (lastWidthIncr < multiple) {
-                        obj.position.x *= multiple;
-                        obj.position.y *= multiple * 1.01;
-                        obj.position.z *= multiple;
-                    } else {
-                        obj.position.x /= multiple;
-                        obj.position.y /= multiple * 1.01;
-                        obj.position.z /= multiple;
-                    }
-                }
-            });
-			multiple += 0.001;
-		}
-	}
 };
 
 let gui = new dat.GUI;
-gui.add(controls, 'addCubes');
-let expWidth = gui.add(controls, 'expWidth').min(1).step(0.001);
-gui.add(controls, 'cubeRotSpd', 0, 0.5).listen();
-gui.add(controls, 'sinWaveFreq', 0.0, 0.1).listen();
-gui.add(controls, 'particleCount').listen();
-gui.add(controls, 'showAxes').listen();
-gui.add(controls, 'showPlane').listen();
-gui.add(controls, 'explode');
-gui.add(controls, 'outputObjects');
-gui.add(controls, 'resetCamera');
-gui.add(controls, 'resetAll');
 
-let lastWidthIncr = 0;
-expWidth.onChange(val => {
-	console.log('val', val);
+let cubesFolder = gui.addFolder('Cubes');
+cubesFolder.add(controls, 'addCubes');
+cubesFolder.add(controls, 'cubeRotSpd', 0, 0.5);
+cubesFolder.add(controls, 'sinWaveFreq', 0.00, 0.05);
+cubesFolder.add(controls, 'exploding').listen();
+cubesFolder.add(controls, 'expand').min(1).step(0.001).onChange(val => {
 	scene.traverse(obj => {
        if (obj instanceof THREE.Mesh && obj != plane ) {
-            if (lastWidthIncr < val) {
+            if (this.lastIncrement < val) {
                 obj.position.x *= val;
-                obj.position.y *= val*1.01;
+                obj.position.y *= val;
                 obj.position.z *= val;
             } else {
                 obj.position.x /= val;
-                obj.position.y /= val*1.01;
+                obj.position.y /= val;
                 obj.position.z /= val;
             }
 		} 
 	});
-	lastWidthIncr = val;
+	this.lastIncrement = val;
 });
+cubesFolder.open();
+
+let lightsFolder = gui.addFolder('Lights');
+lightsFolder.addColor(controls, 'ambientColor').onChange(e => ambientLight.color = new THREE.Color(e));
+lightsFolder.add(controls, 'ambientLightON').listen();
+lightsFolder.add(controls, 'spotLightON').listen();
+lightsFolder.open();
+
+let helpersFolder = gui.addFolder('Helpers');
+helpersFolder.add(controls, 'showAxes').listen();
+helpersFolder.add(controls, 'showPlane').listen();
+helpersFolder.add(controls, 'showSpotLight').listen();
+
+gui.add(controls, 'particleCount').listen();
+gui.add(controls, 'rotateCamera').listen();
+gui.add(controls, 'outputObjects');
+gui.add(controls, 'resetCamera');
+gui.add(controls, 'resetAll');
+
+// END CONTROLS ================================================================
 
 let step = 0;
 let stats = initStats();
 window.addEventListener('resize', onResize, false);
-
 render();
+
+function render(timestamp) {
+	if (controls.exploding) explodeParticles();
+	OrbitControls.autoRotate = (controls.rotateCamera) ? true : false;
+
+	// toggle lights
+	controls.ambientLightON ? scene.add(ambientLight) : scene.remove(ambientLight);
+	controls.spotLightON ? scene.add(spotLight) : scene.remove(spotLight);
+
+	// toggle helpers
+	controls.showAxes ? scene.add(axisHelper) : scene.remove(axisHelper);
+    controls.showPlane ? scene.add(plane) : scene.remove(plane);
+    controls.showSpotLight ? scene.add(cameraHelper) : scene.remove(cameraHelper);
+
+
+	stats.begin();
+	stats.end();
+
+	animate();	
+	OrbitControls.update();
+	renderer.render(scene, camera);
+	requestAnimationFrame(render);
+}
 
 function initStats() {
     let stats = new Stats();
@@ -196,34 +232,29 @@ function initStats() {
     return stats;
 }
 
-function render() {
-	step += controls.sinWaveFreq;
-	OrbitControls.update();
-	renderer.render(scene, camera);
+function animate() {
+	while (cubes.length < 500) controls.addCubes();
+    step += controls.sinWaveFreq;
+    scene.traverse(obj => {
+        if (obj instanceof THREE.Mesh && obj != plane) {
+            obj.rotation.x += controls.cubeRotSpd;
+            obj.rotation.y += controls.cubeRotSpd;
+            if (controls.sinWaveFreq > 0) obj.position.y += Math.sin(step);
+        }
+    });
+}
 
-	if (controls.showAxes) {
-		scene.add(axisHelpers);
-	} else {
-		scene.remove(axisHelpers);
-	}
-
-	if (controls.showPlane) {
-		scene.add(plane);
-	} else {
-		scene.remove(plane);
-	}
-
-	scene.traverse(obj => {
-       if (obj instanceof THREE.Mesh && obj != plane ) {
-       	obj.rotation.x+=controls.cubeRotSpd;
-         obj.rotation.y+=controls.cubeRotSpd;
-         if (controls.sinWaveFreq > 0) obj.position.y+=Math.sin(step);
-		} 
-	});
-
-	stats.begin();
-	stats.end();
-	requestAnimationFrame(render);
+function explodeParticles() {
+    scene.traverse(obj => {
+        if (obj instanceof THREE.Mesh && obj != plane) {
+            obj.rotation.x += 1;
+            obj.rotation.y += 1;
+            obj.rotation.z += 1;
+            obj.position.x *= 1.01;
+            obj.position.y *= 1.01;
+            obj.position.z *= 1.01;
+        }
+    });
 }
 
 function onResize() {
